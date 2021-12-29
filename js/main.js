e6dfec86e8319cefbed1600a7faf3d9476ab73bf
Axel León <yka992@hotmail.com>
2021-12-29 17:52:23 -0600

    Indicador de carga al generar PDF

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ const guestForm = document.getElementById("guest-form");
 
 guestForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  // Set button to loading mode
+  const submitButton = document.getElementById("submit-button");
+  submitButton.innerHTML = "LOADING...";
+  submitButton.disabled = true;
+
   const firstName = document.getElementById("guest-form-first-name").value;
   const lastName = document.getElementById("guest-form-last-name").value;
   const email = document.getElementById("guest-form-email").value;
@@ -19,7 +25,12 @@ guestForm.addEventListener("submit", (e) => {
   })
     .then((response) => response.blob())
     .then((blob) => {
-      const objectUrl = URL.createObjectURL(blob);
+      blob.type = "application/pdf";
+      const objectUrl = URL.createObjectURL(blob, {
+        type: "application/pdf",
+      });
+      submitButton.innerHTML = "ENTER";
+      submitButton.disabled = false;
       window.location.href = objectUrl;
     });
 });
